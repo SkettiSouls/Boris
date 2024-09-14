a9702a8d81170418b0e2fbea4a95ed727b299e79
SkettiSouls <skettisouls@gmail.com>
2024-09-14 18:28:46 -0500

    Summon: Add random dms

--- src/commands/summon.rs
+++ src/commands/summon.rs
@@ -12,14 +12,32 @@ use serenity::{
     utils::MessageBuilder,
 };
 
+use rand::seq::SliceRandom;
+
 #[command]
 async fn summon(ctx: &Context, msg: &Message) -> CommandResult {
     if !msg.mentions.is_empty() {
         for user in &msg.mentions {
             if !user.bot {
+                let message_pool = [
+                    "Get on stink ass",
+                    "Holy shit man Humpty Dumpty just fell off the wall, I dont know what to do man bring a horse or something",
+                    "Arise",
+                    "My fellow americans, uhhh, get on the game (obama)",
+                    r#"in your DMs. straight up "summoning it". and by "it" well, let's justr say.  My homie"#,
+                    "Its time",
+                    "PLEEEEEEEEEEEEEEEAAAAASE PLEASE PLEEASE PLEEEEEEEEEEEEEEEEEEEEEEEAAAAAAAAAAAASE",
+                    "HELP IN GAIA",
+                    "Wakey wakey",
+                    "I may seem calm but in my mind I've killed you three times fucking join you dickhead",
+                ];
+
+                let message = CreateMessage::new().content(
+                    message_pool.choose(&mut rand::thread_rng())
+                    .unwrap()
+                    .to_string());
+
                 let nick = User::nick_in(&user, &ctx.http, &msg.guild_id.unwrap()).await;
-                // TODO: Pick from a list of messages
-                let message = CreateMessage::new().content("Get on stink ass");
                 // TODO?: Add DM count
                 let _dm_user = User::dm(&user, &ctx.http, message).await;
                 let response = MessageBuilder::new()
